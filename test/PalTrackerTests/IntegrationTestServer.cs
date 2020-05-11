753371264e05e6fee74719857186cf3ce1f7126a
Anita-Majhi <anita.majhi@gmail.com>
2020-05-11 10:51:33 +0000

    Externalized Configuration

--- test/PalTrackerTests/IntegrationTestServer.cs
+++ test/PalTrackerTests/IntegrationTestServer.cs
@@ -1,12 +1,22 @@
 ﻿using Microsoft.AspNetCore.TestHost;
 using Microsoft.AspNetCore.Mvc.Testing;
+using System.Threading.Tasks;
 using PalTracker;
 
 namespace PalTrackerTests
 {
     public static class IntegrationTestServer
     {
-        public static TestServer Start() =>
-            new WebApplicationFactory<Program>().Server;
+    //  public static TestServer Start() =>
+    //         new WebApplicationFactory<Program>().Server;
+        public static TestServer Start()
+        {
+            if (string.IsNullOrEmpty(System.Environment.GetEnvironmentVariable("WELCOME_MESSAGE")))
+            {
+                System.Environment.SetEnvironmentVariable("WELCOME_MESSAGE", "Default message");
+            }
+
+            return new WebApplicationFactory<Program>().Server;
+        }
     }
 }
