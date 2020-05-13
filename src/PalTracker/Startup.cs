652a6b6989fc203a4b013e1e80a56663332d7124
Anita-Majhi <anita.majhi@gmail.com>
2020-05-13 04:42:15 +0000

    Health check pipeline

--- src/PalTracker/Startup.cs
+++ src/PalTracker/Startup.cs
@@ -11,6 +11,13 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Steeltoe.CloudFoundry.Connector.MySql.EFCore;
+using Steeltoe.Management.CloudFoundry;
+using Steeltoe.Management.Endpoint.CloudFoundry;
+using Steeltoe.Common.HealthChecks;
+using Steeltoe.Management.Endpoint.Info;
+using Steeltoe.Management.Hypermedia;
+ 
+using Steeltoe.Management.Endpoint;
 
 namespace PalTracker
 {
@@ -26,8 +33,13 @@ namespace PalTracker
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers();
+            services.AddSingleton<IInfoContributor, TimeEntryInfoContributor>(); 
+            services.AddSingleton<IOperationCounter<TimeEntry>, OperationCounter<TimeEntry>>(); 
+            services.AddScoped<IHealthContributor, TimeEntryHealthContributor>(); 
+            services.AddCloudFoundryActuators(Configuration, MediaTypeVersion.V2, ActuatorContext.ActuatorAndCloudFoundry);
                        var message = Configuration.GetValue<string>("WELCOME_MESSAGE");
+            
+            services.AddControllers();
            if (string.IsNullOrEmpty(message))
            {
                throw new ApplicationException("WELCOME_MESSAGE not configured.");
@@ -47,10 +59,12 @@ namespace PalTracker
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
+            app.UseCloudFoundryActuators(MediaTypeVersion.V2, ActuatorContext.ActuatorAndCloudFoundry); 
 
             app.UseHttpsRedirection();
 
